08a7bef5e6b2f0abee071e37d4b30232cbac0af0
Aqil Jawadal <aqiljawadalfurqon@gmail.com>
2021-10-14 15:50:23 +0700

    Praktikum Pertemuan 1
    review ulang ngoding java

---
+++ src/modul0/Tipe.java
@@ -0,0 +1,23 @@
+package modul0;
+
+public class Tipe {
+    public static void main(String[] args) {
+        long data1 = 767226531;
+        int data2 = 22355641;
+        short data3 = 714;
+        byte data4 = 34;
+        double data5 = 4.967;
+        float data6 = (float) 1.733;
+        char data7 = 'C';
+        boolean data8 = true;
+
+        System.out.println(data1);
+        System.out.println(data2);
+        System.out.println(data3);
+        System.out.println(data4);
+        System.out.println(data5);
+        System.out.println(data6);
+        System.out.println(data7);
+        System.out.println(data8);
+    }
+}
