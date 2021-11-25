c031050591b2c2fc1c23e617224b0498af77f988
Aqil Jawadal <aqiljawadalfurqon@gmail.com>
2021-11-25 10:05:02 +0700

    Modul7

---
+++ src/modul7/percobaan/Mahasiswa.java
@@ -0,0 +1,11 @@
+package modul7.percobaan;
+
+public class Mahasiswa {
+    public void info() {
+        System.out.println("Haloo.. Aku Mahasiswa!");
+    }
+
+    public void info(String nama) {
+        System.out.println("Haloo.. Namaku " + nama);
+    }
+}
